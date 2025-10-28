1b3ba68f6ff1617c903ba8e7254da14a04e93cf0
Mikołaj <mikolajkock@gmail.com>
2025-10-28 21:42:44 +0100

    initial

---
+++ src/store/tasks.ts
@@ -0,0 +1,20 @@
+export const tasks = [
+  {
+    id: 1,
+    title: 'Make ToDo App',
+    description: 'Simple to do application using React and TypeScript',
+    completed: false,
+    },
+    {
+    id: 2,
+    title: 'Make booking app',
+    description: 'Try making a large booking application using React and TypeScript, integrate with payment gateway, calendar API, email service and supabase',
+    completed: false,
+    },
+    {
+    id: 3,
+    title: 'Make help center app',
+    description: 'Application for help center with ticketing system, live chat and knowledge base using React and TypeScript for companies',
+    completed: false,
+    }
+]
